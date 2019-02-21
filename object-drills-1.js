b4328040863aa149811825d820fc00802255e92b
tmmgeek <2015rpro@gmail.com>
2019-02-21 11:52:17 -0500

    completed task 3-David

--- object-drills-1.js
+++ object-drills-1.js
@@ -28,3 +28,19 @@ function personMaker() {
   };
   return person.fullName();
 }
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  // your code here
+  delete obj.foo;
+  delete obj.bar;
+}
+//keyDeleter(sampleObj);
+
+
